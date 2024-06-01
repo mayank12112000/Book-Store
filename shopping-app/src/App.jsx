22f7fcc1a1715e230968d4bab0501d59071567b2
Mayank  Keshari <mayank.keshari.93@gmail.com>
2024-06-01 11:35:48 +0530

    shopping app setup and sidebar

---
+++ shopping-app/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import SideBar from './navbar/SideBar'
+
+function App() {
+  return (
+    <SideBar/>
+  )
+}
+
+export default App
---
+++ shopping-app/src/navbar/SideBar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export default function SideBar() {
+    return (
+        <div className="flex-shrink-0 p-3" style={{ width: "20rem"}}>
+            <button  href="/" className="btn btn-outline-primary " style={{textAlign:"center"}}>
+                <span className="fs-5 fw-semibold">Add New Product</span>
+            </button>
+            <hr />
+            <ul className="list-unstyled ps-0">
+                <li className="mb-1">
+                    <button className="btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed" data-bs-toggle="collapse" data-bs-target="#home-collapse" aria-expanded="false">
+                       <h6>Category Filter </h6>
+                    </button>
+                    <div className="collapse" id="home-collapse" >
+                        <ul className="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+                            <li style={{ marginLeft: "20px", padding: "3px 8px" }}><a href="/" className="link-body-emphasis d-inline-flex text-decoration-none rounded">cat 1</a></li>
+                            <li style={{ marginLeft: "20px", padding: "3px 8px" }}><a href="/" className="link-body-emphasis d-inline-flex text-decoration-none rounded">cat 2</a></li>
+                            <li style={{ marginLeft: "20px", padding: "3px 8px" }}><a href="/" className="link-body-emphasis d-inline-flex text-decoration-none rounded">cat 3</a></li>
+                        </ul>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    )
+}
