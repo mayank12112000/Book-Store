accbec44dd85add6d6c6be4a17fd651409122e5f
Mayank  Keshari <mayank.keshari.93@gmail.com>
2024-06-04 23:56:27 +0530

    routing correction

--- shopping-app/src/components/ProductCard.jsx
+++ shopping-app/src/components/ProductCard.jsx
@@ -5,7 +5,7 @@ export default function ProductCard({ product }) {
     return (
         <>
         <div className='container m-1' style={{color:"inherit",textDecoration:"none", overflow: "hidden", height: "18rem", width: "18rem", border: "2px solid black" }}>
-            <Link to={`./product/${product.id}`}>
+            <Link to={`/product/${product.id}`}>
                 <div className='zoom-container' style={{ margin:"5px",height: "80%", backgroundImage: `url("${product.image}")`, backgroundSize: "contain", backgroundPosition: "center", backgroundRepeat: "no-repeat" }}></div>
             </Link>
                 <p style={{fontWeight:"500"}}>{product.title}</p>
