f6bcbcbcd09e0aa5442e0f2a1d0029681076722b
Mayank  Keshari <mayank.keshari.93@gmail.com>
2024-06-15 19:45:48 +0530

    login form

--- book store/src/features/auth/LoginPage.jsx
+++ book store/src/features/auth/LoginPage.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import "./loginpage.css"
+import { Link } from 'react-router-dom'
 
 export default function LoginPage() {
+
+  // login submit handler
+  const onSubmitHandler = (e) => {
+    e.preventDefault()
+  }
+
+
   return (
-    <div>
-      login page
+    <div className='loginpage'>
+      <div className="wrapper fadeInDown">
+        <div id="formContent">
+          <br /><h2>Login</h2> <br />
+          <form onSubmit={onSubmitHandler}>
+            <input type="text" id="login" className="fadeIn second" name="login" placeholder="login" />
+            <input type="text" id="password" className="fadeIn third" name="login" placeholder="password" />
+            <input type="submit" className="fadeIn fourth" value="Log In" />
+          </form>
+          <div id="formFooter">
+            <Link to={"/signup"}>
+            <div className="underlineHover" >{'Create Account>'}</div>
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
---
+++ book store/src/features/auth/authSlice.js
@@ -0,0 +1,20 @@
+import {createSlice} from "@reduxjs/toolkit"
+const initialState = {
+    user:null,
+    status:"idel", // idle loading succeeded failed
+    error:null
+}
+
+const authSlice = createSlice({
+    name:"auth",
+    initialState,
+    reducers:{
+        logout:(state)=>{
+            state.user = null; // logout reducer 
+        }
+    },
+   
+})
+
+export default authSlice.reducers
+export const {logout} = authSlice.actions
