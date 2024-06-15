21de00f7a7aa5021ee5a79401a4838ad59c85fed
Mayank  Keshari <mayank.keshari.93@gmail.com>
2024-06-15 11:52:06 +0530

    home page image setup

--- book store/src/pages/HomePage.jsx
+++ book store/src/pages/HomePage.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
-
+import "./homePage.css"
 const HomePage = () => {
   return (
     <div>
-      <h1>Home Page</h1>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. At temporibus dignissimos reiciendis eos, minus laboriosam enim eligendi quaerat odio repudiandae? Velit, repudiandae cumque. Unde eius dolore, quis tempora nesciunt assumenda.</p>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. At temporibus dignissimos reiciendis eos, minus laboriosam enim eligendi quaerat odio repudiandae? Velit, repudiandae cumque. Unde eius dolore, quis tempora nesciunt assumenda.</p>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. At temporibus dignissimos reiciendis eos, minus laboriosam enim eligendi quaerat odio repudiandae? Velit, repudiandae cumque. Unde eius dolore, quis tempora nesciunt assumenda.</p>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. At temporibus dignissimos reiciendis eos, minus laboriosam enim eligendi quaerat odio repudiandae? Velit, repudiandae cumque. Unde eius dolore, quis tempora nesciunt assumenda.</p>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. At temporibus dignissimos reiciendis eos, minus laboriosam enim eligendi quaerat odio repudiandae? Velit, repudiandae cumque. Unde eius dolore, quis tempora nesciunt assumenda.</p>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. At temporibus dignissimos reiciendis eos, minus laboriosam enim eligendi quaerat odio repudiandae? Velit, repudiandae cumque. Unde eius dolore, quis tempora nesciunt assumenda.</p>
+      <div className='image-box'>
+      <div className='image-container'>
+        <div className='text-area'>
+          <h2>Welcome to Bookify!</h2>
+          <h1>Find all your knowledge here!</h1>
+          <button className='btn'>Shop now</button>
+        </div>
+      </div>
+
+      </div>
       <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. At temporibus dignissimos reiciendis eos, minus laboriosam enim eligendi quaerat odio repudiandae? Velit, repudiandae cumque. Unde eius dolore, quis tempora nesciunt assumenda.</p>
       <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. At temporibus dignissimos reiciendis eos, minus laboriosam enim eligendi quaerat odio repudiandae? Velit, repudiandae cumque. Unde eius dolore, quis tempora nesciunt assumenda.</p>
       <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. At temporibus dignissimos reiciendis eos, minus laboriosam enim eligendi quaerat odio repudiandae? Velit, repudiandae cumque. Unde eius dolore, quis tempora nesciunt assumenda.</p>
