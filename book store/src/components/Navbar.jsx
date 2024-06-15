57d3d069a1e30ab789ee602673b6a508b4eb5847
Mayank  Keshari <mayank.keshari.93@gmail.com>
2024-06-15 11:24:32 +0530

    navbar set

--- book store/src/components/Navbar.jsx
+++ book store/src/components/Navbar.jsx
@@ -1,42 +1,35 @@
-// Navbar.js
-
+import "./Navbar.css";
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav className="navbar navbar-expand-lg navbar-light">
       <div className="container">
-        <Link to="/" className="navbar-brand">Bookify</Link>
+        <Link to="/" className="navbar-brand"><b>Bookify</b></Link>
 
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ml-auto">
-            <li className="nav-item">
-              <Link to="/" className="nav-link">Home</Link>
+        <div className="search-bar" >
+          <span class=" search-icon"><SearchOutlinedIcon/></span>
+          <input className="search-input" placeholder="search book" type="text" />
+        </div>
+        <div className="d-flex">
+          <ul className="d-flex flex-row p-1 navbar-nav ml-auto">
+            <li className="nav-item mx-2">
+              <Link to="/cart" className="nav-link"><ShoppingCartOutlinedIcon/></Link>
             </li>
-            <li className="nav-item">
-              <Link to="/products" className="nav-link">Products</Link>
+            <li className="nav-item mx-2">
+              <Link to="/wishlist" className="nav-link"><FavoriteBorderOutlinedIcon/></Link>
             </li>
-            <li className="nav-item">
-              <Link to="/cart" className="nav-link">Cart</Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/wishlist" className="nav-link">Wishlist</Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/login" className="nav-link">Login</Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/signup" className="nav-link">Sign Up</Link>
+            <li className="nav-item mx-2">
+              <Link to="/login" className="nav-link"><AccountCircleOutlinedIcon/></Link>
             </li>
           </ul>
         </div>
       </div>
+      {/* <AccountCircleOutlinedIcon /> */}
     </nav>
   );
 };
--- book store/src/features/products/ProductLIstPage.jsx
+++ book store/src/features/products/ProductLIstPage.jsx
@@ -1,6 +1,5 @@
 import React, { useContext, useEffect } from 'react';
 import ProductCard from '../../components/ProductCard';
-import axios from 'axios';
 
 const ProductListPage = () => {
 
