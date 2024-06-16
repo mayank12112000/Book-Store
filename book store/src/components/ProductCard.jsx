6db71b5c170f175e54e4c34b0dc5f885823bf380
Mayank  Keshari <mayank.keshari.93@gmail.com>
2024-06-16 17:05:34 +0530

    product listing

--- book store/src/components/ProductCard.jsx
+++ book store/src/components/ProductCard.jsx
@@ -3,28 +3,28 @@ import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 import StarBorderIcon from '@mui/icons-material/StarBorder';
 import "./ProductCard.css"
-const ProductCard = () => {
+const ProductCard = ({book}) => {
 
 
   return (
    <div className='product-card-box'>
     <div className='product-img-card'>
-      <img className='card-img' src="https://rukminim1.flixcart.com/image/612/612/kufuikw0/book/0/6/q/ikigai-original-imag7kc7q5ugy2km.jpeg?q=70" alt="ikigai" />
+      <img className='card-img' src={book.imgLink} alt="ikigai" />
       <span className='wishlist-icon'><FavoriteBorderIcon/></span>
     </div>
     <div className='info'>
       <div className='card-title'>
-        <h3 className='card-title-header'>heading</h3>
+        <h3 className='card-title-header'>{book.title}</h3>
         <div className="rating">
           <div className='rated'>3</div>
           <StarBorderIcon className='star'/>
         </div>
       </div>
-      <p className='author'>author</p>
+      <p className='author'>{book.author}</p>
       <div className='price'>
-        <p className='disc-price'>600</p>
-        <p className='actual-price'>1500</p>
-        <p className='price-percentage'>{"(60% off)"}</p>
+        <p className='disc-price'>{book.discountedPrice}</p>
+        <p className='actual-price'>{book.price}</p>
+        <p className='price-percentage'>{`(${book.discountPercentage}% off)`}</p>
       </div>
       <button className='btn default add-cart'>
         <ShoppingCartOutlinedIcon/>Add to cart
--- book store/src/features/products/ProductLIstPage.jsx
+++ book store/src/features/products/ProductLIstPage.jsx
@@ -1,8 +1,9 @@
 import React, { useContext, useEffect } from 'react';
-import ProductCard from '../../components/ProductCard';
+import Loader from '../../components/Loader';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchProductsAsync } from './productsSlice';
-
+import ProductCard from "../../components/ProductCard"
+import "./productList.css"
 const ProductListPage = () => {
   const {books,error,status} = useSelector((state)=>state.products)
   const dispatch = useDispatch()
@@ -15,8 +16,18 @@ const ProductListPage = () => {
   },[dispatch])
 
   return (
-    <div>
-      product list page
+    <div className='product-list-container'>
+      <div className="product-list-header">
+        showing all products
+      </div>
+      <div className="responsive-grid">
+      {(error !== null || status==="failed")?
+      <Loader/>
+      :(
+        books.map((book)=><div key={book.id} className='card'><ProductCard book={book}/></div>)
+      )}
+      </div>
+    
     </div>
   );
 };
