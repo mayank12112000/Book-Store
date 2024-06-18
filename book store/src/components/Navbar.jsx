eb72c01a18cd6cf773890148243ee4749ac1ebcd
Mayank  Keshari <mayank.keshari.93@gmail.com>
2024-06-18 19:33:58 +0530

    filter offcanvas

--- book store/src/components/Navbar.jsx
+++ book store/src/components/Navbar.jsx
@@ -1,18 +1,19 @@
 import "./Navbar.css";
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 const Navbar = () => {
+  const {pathname} = useLocation()
   return (
     <nav className="navbar navbar-expand-lg navbar-light">
       <div className="navbar-container">
         <div className="home-hamburger">
-          <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasWithBothOptions" aria-controls="offcanvasWithBothOptions" aria-expanded="false" aria-label="Toggle navigation">
+          {pathname==="/products" && <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasWithBothOptions" aria-controls="offcanvasWithBothOptions" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
-          </button>
+          </button>}
           <Link to="/" className="navbar-brand"><b>Bookify</b></Link>
         </div>
 
--- book store/src/components/OffCanvas/Filters.jsx
+++ book store/src/components/OffCanvas/Filters.jsx
@@ -1,15 +1,75 @@
 import React from 'react'
-
+import "./filters.css"
+import { useSelector } from 'react-redux'
 export default function Filters() {
+  const { categories } = useSelector((state) => state.categories)
+  console.log(categories)
   return (
-    <div className="offcanvas my-5 offcanvas-start" data-bs-scroll="true" tabindex="-1" id="offcanvasWithBothOptions" aria-labelledby="offcanvasWithBothOptionsLabel">
-          <div className="offcanvas-header">
-            <h5 className="offcanvas-title" id="offcanvasWithBothOptionsLabel">Backdrop with scrolling</h5>
-            <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+    <div className="offcanvas offcanvas-start custom-offcanvas" data-bs-scroll="true" tabIndex="-1" id="offcanvasWithBothOptions" aria-labelledby="offcanvasWithBothOptionsLabel">
+      <div className="offcanvas-header">
+        <h5 className="offcanvas-title" id="offcanvasWithBothOptionsLabel">Filters</h5>
+        <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+      <div className="offcanvas-body " >
+        <div className="filter-price">
+          <b> <small>price:</small></b>
+          <label htmlFor="customRange1" className="form-label price-ranges">
+            <p> <small>100</small></p>
+            <p> <small>500</small></p>
+            <p> <small>1000</small></p>
+          </label>
+          <input min={100} max={1000} type="range" className="form-range" id="customRange1" />
+        </div> <br />
+
+        <div className='filter-category'>
+          <b> <small>Category:</small></b>
+          {categories && categories.map((category, idx) => {
+            return (
+              <div key={idx} className="btn-group" role="group" aria-label="Basic checkbox toggle button group">
+                <input type="checkbox" id={category.name} />
+                <label className="" htmlFor={category.name}><small> {category.name}</small></label>
+              </div>
+            )
+          })}
+        </div> <br />
+
+        <div className="filter-rating">
+          <div><b><small>Rating:</small></b></div>
+          <div>
+            <input type="radio" id='1' value={1}/>
+            <label htmlFor="1"><small>1 star & above</small></label>
           </div>
-          <div className="offcanvas-body " >
-            <p>Try scrolling the rest of the page to see this option in action.</p>
+          <div>
+            <input type="radio" id='2' value={2}/>
+            <label htmlFor="2"><small>2 star & above</small></label>
           </div>
+          <div>
+            <input type="radio" id='3' value={3}/>
+            <label htmlFor="3"><small>3 star & above</small></label>
+          </div>
+          <div>
+            <input type="radio" id='4' value={4}/>
+            <label htmlFor="4"><small>4 star & above</small></label>
+          </div>
+          <div>
+            <input type="radio" id='5' value={5}/>
+            <label htmlFor="5"><small>5 star</small></label>
+          </div>
+        </div> <br />
+        
+        <div className='sort-by'>
+          <div><small> <b>Sort by:</b></small></div>
+          <div>
+            <input type="radio" id='5' value={5}/>
+            <label htmlFor="5"><small>Price-low to high</small></label>
+          </div>
+          <div>
+            <input type="radio" id='5' value={5}/>
+            <label htmlFor="5"><small>Price-high to low</small></label>
+          </div>
+
         </div>
+      </div>
+    </div>
   )
 }
