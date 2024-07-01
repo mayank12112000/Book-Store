4d6f6ef935b6f702b63276ad6e912b639a731f7e
Mayank  Keshari <mayank.keshari.93@gmail.com>
2024-07-01 23:57:51 +0530

    navlink addition

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import "./Navbar.css";
 import React from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import { Link, NavLink, useLocation } from 'react-router-dom';
 import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
@@ -34,27 +34,27 @@ const Navbar = () => {
         <div className="d-flex">
           <ul className="d-flex flex-row p-1 navbar-nav ml-auto">
             <li className="nav-item mx-2">
-              <Link to="/cart" className="navbar-icons nav-link">
+              <NavLink to="/cart" activeClassName="active-nav-link" className="navbar-icons nav-link">
                 <div className="position-relative">
                   <ShoppingCartOutlinedIcon />
                   <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
                     {cart.length>0 && cart.length}
                   </span>
                 </div>
-              </Link>
+              </NavLink>
             </li>
             <li className="nav-item mx-2">
-              <Link to="/wishlist" className="navbar-icons nav-link">
+              <NavLink to="/wishlist" activeClassName="active-nav-link" className="navbar-icons nav-link">
                 <div className="position-relative">
                   <FavoriteBorderOutlinedIcon />
                   <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
                     {wishList.length>0 && wishList.length}
                   </span>
                 </div>
-              </Link>
+              </NavLink>
             </li>
             <li className="nav-item mx-2">
-              <Link to="/user_profile" className="navbar-icons nav-link"><AccountCircleOutlinedIcon /></Link>
+              <NavLink to="/user_profile" activeClassName="active-nav-link" className="navbar-icons nav-link"><AccountCircleOutlinedIcon /></NavLink>
             </li>
           </ul>
         </div>
